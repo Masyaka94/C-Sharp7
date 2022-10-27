3df06a73251181cd2cc900f2607cfff7330f5f50
masyaka.94 <masyaka.94@gmail.com>
2022-10-27 20:12:04 +0500

    dobavlena zadacha 50

--- Zadacha50/Program.cs
+++ Zadacha50/Program.cs
@@ -4,36 +4,35 @@
 
 void FillArray(int[,] matr)
 {
-Random rnd = new Random();
-for (int i = 0; i<matr.GetLength(0); i++)
-	{
-	for (int j = 0; j<matr.GetLength(1); j++)
-		{
-		matr[i,j] = rnd.Next(-100,100)
-		}
-	}
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(-100, 100);    
+        }
+    }
 }
 
 void PrintArray(int[,] matr)
 {
-for (int rows = 0; matrix.GetLength(0); rows++)
-{
-	for (int columns = 0; matrix.GetLength(1); columns++)
-		{
-		Console.Write($"{matrix[rows,columns]} ";
-		}
-	Console.WriteLine();
-}
+    for (int rows = 0; rows<matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns<matr.GetLength(1); columns++)
+        {
+            Console.Write($"{matr[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
 }
 
 Console.Write($"Введите количество строк в массиве: ");
-int m = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine()!);
 
 Console.Write($"Введите количество столбцов в массиве: ");
-int n = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine()!);
 
-int [,] matrix = new int[m,n]
-
+int[,] matrix = new int[m, n];
 FillArray(matrix);
 PrintArray(matrix);
 
@@ -41,12 +40,13 @@ Console.Write($"Введите первый индекс элемента: ");
 int indexStr = int.Parse(Console.ReadLine());
 
 Console.Write($"Введите второй индекс элемента: ");
-int indexStl = int.Parse(Console.ReadLine());
+int indexStl = int.Parse(Console.ReadLine()!);
 
-if (indexStr<=m && indexStl<=n)
+if (indexStr <= m-1 && indexStl <= n-1)
 {
-Console.Write($"Элемент: {matrix[indexStr,indexStl]} ");
+    Console.Write($"Элемент: {matrix[indexStr, indexStl]} ");
 }
 else
 {
-Console.Write($"Заданные индексы вне массива");
+    Console.Write($"Заданные индексы вне массива ");
+}
